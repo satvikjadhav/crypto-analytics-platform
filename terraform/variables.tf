e4cf9de6b7d22136d91a099b7031b52bd545bbde
satvikjadhav <satvikj5@gmail.com>
2026-06-18 14:25:50 -0500

    adding snowflake variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,3 +14,25 @@ variable "initials" {
   description = "Appended to globally unique names."
   type        = string
 }
+
+variable "snowflake_account" {
+  description = "Snowflake account identifier (e.g. xy12345.us-east-1)"
+  type        = string
+}
+
+variable "snowflake_user" {
+  description = "Snowflake admin user for Terraform (your login user)"
+  type        = string
+}
+
+variable "snowflake_password" {
+  description = "Snowflake admin password for Terraform"
+  type        = string
+  sensitive   = true
+}
+
+variable "snowflake_pipeline_password" {
+  description = "Password for the dedicated pipeline service user"
+  type        = string
+  sensitive   = true
+}
