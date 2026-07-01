9a2a38b369cf447a0ef2363143224e3b2811d7e4
satvikjadhav <satvikj5@gmail.com>
2026-07-01 15:38:41 -0500

    adding schema, parsing json and adding a date partition column

--- spark_jobs/streaming_ingest.py
+++ spark_jobs/streaming_ingest.py
@@ -1,11 +1,22 @@
 import os
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.types import StructType, StructField, StringType, LongType, BooleanType, DoubleType
 
 PACKAGES = ",".join([
     "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0",
     "io.delta:delta-core_2.12:2.4.0",
     "org.apache.hadoop:hadoop-aws:3.3.4",
     "com.amazonaws:aws-java-sdk-bundle:1.12.262",
+])
+
+trade_schema = StructType([
+    StructField("symbol",         StringType(),  False),
+    StructField("price",          DoubleType(),  False),
+    StructField("quantity",       DoubleType(),  False),
+    StructField("trade_time",     LongType(),    False),
+    StructField("is_buyer_maker", BooleanType(), False),
+    StructField("ingestion_ts",   LongType(),    False),
 ])
 
 spark = (
@@ -39,3 +50,14 @@ raw_stream = (
     .option("maxOffsetsPerTrigger", 50_000)
     .load()
 )
+
+parsed = (
+    raw_stream
+    .select(F.from_json(F.col("value").cast("string"), trade_schema).alias("d"))
+    .select("d.*")
+    .withColumn("trade_timestamp", (F.col("trade_time") / 1000).cast("timestamp"))
+    .withColumn("date", F.to_date("trade_timestamp"))
+)
+
+good = parsed.filter(F.col("symbol").isNotNull())
+bad = parsed.filter(F.col("symbol").isNull())
